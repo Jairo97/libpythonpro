fa55041d3ca73c8ac2c7f901b6c7823a7371a89e
Jairo97 <oliveirajairon10@gmail.com>
2022-05-01 08:24:32 -0300

    Adicionada flake8 como dependencia

--- README.md
+++ README.md
@@ -5,6 +5,17 @@ link para o curso [Python Pro](https://pythonpro.com.br/)
 
 Suportada versão 3 de Python
 
+Para instalar:
+```commandline
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements-dev.txt
+```
+
+Para conferir qualidade de codigo:
+```commandline
+flake8
+```
 Topicos a serem abordados:
 1. Git
 2. Virtualenv
---
+++ libpythonpro/github_api.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    """
+    Buscar o avatar de um usuario no Github
+    :param usuario: str com o nome de usuario no github
+    :return: str com o link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    r = requests.get(url)
+    return r.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(buscar_avatar('jairo97'))
