163eed95b925031aac41ee8abbcc8a501d5251d1
Jairo97 <oliveirajairon10@gmail.com>
2022-05-27 21:48:10 -0300

    Escrevendo Mocks - Pytest-Mock

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,14 +1,15 @@
 from libpythonpro.spam.modelos import Usuario
 
 
-def test_salvar_usuarios(sessao):
-    usuario = Usuario(nome='Jairo')
+def test_salvar_usuario(sessao):
+    usuario = Usuario(nome='Jairo', email='oliveirajairon10@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Jairo'), Usuario(nome='Oliveira')]
+    usuarios = [Usuario(nome='Jairo', email='oliveirajairon10@gmail.com'),
+                Usuario(nome='Oliveira', email='oliveirajairon10@gmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
